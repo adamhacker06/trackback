31854297efdf4b38e93951dc096982b4ac60ae24
Adam Hacker <adamhack2006@gmail.com>
2025-07-19 15:02:47 -0700

    Added SpotifyController which deals with session and app managers. Added url schema to info

---
+++ trackback/Controllers/SpotifyController.swift
@@ -0,0 +1,82 @@
+//
+//  SpotifyController.swift
+//  trackback
+//
+//  Created by Adam Hacker on 7/19/25.
+//
+
+import SwiftUI
+import Combine
+import SpotifyiOS
+
+class SpotifyController: NSObject, ObservableObject {
+    
+    let spotifyClientID: String = ""
+    let spotifyRedirectURL: URL = URL(string: "")!
+    
+    @Published var accessToken: String?
+    @Published var isAuthorized: Bool = false
+    
+    private var appRemote: SPTAppRemote?
+    private var sessionManager: SPTSessionManager?
+    
+    private var connectionRequest: AnyCancellable?
+    
+    func connect() {
+        let configuration = SPTConfiguration(
+            clientID: spotifyClientID,
+            redirectURL: spotifyRedirectURL
+        )
+        
+        configuration.playURI = ""
+        self.sessionManager = SPTSessionManager(configuration: configuration, delegate: self)
+        
+        let scopes: SPTScope = [.userReadEmail, .userReadPrivate, .userTopRead, .playlistReadPrivate, .streaming]
+        
+    }
+    
+    func disconnect() {
+        
+        if appRemote?.isConnected == true {
+            appRemote?.disconnect()
+        }
+        self.isAuthorized = false
+        self.accessToken = nil
+        
+    }
+    
+    func handleURL(_ url: URL) {
+        self.sessionManager?.application(UIApplication.shared, open: url, options: [:])
+    }
+    
+}
+
+extension SpotifyController: SPTSessionManagerDelegate {
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("SUCCESS: Session initatied")
+        self.accessToken = session.accessToken
+        self.isAuthorized = true
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: any Error) {
+        print("FAILURE: Session failed to initiatie: \(error.localizedDescription)")
+        self.isAuthorized = false
+    }
+    
+}
+
+extension SpotifyController: SPTAppRemoteDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("SUCCESS: App remote connected")
+        self.appRemote = appRemote
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: (any Error)?) {
+        print("FAILURE: Failed to conneect to app remote")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: (any Error)?) {
+        print("NOTIF: App remote disconnected")
+    }
+}
